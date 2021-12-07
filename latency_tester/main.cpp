b0a24f38bd6254e520a1978595eca7d975301772
Andreas Schmid <andreas.schmid@ur.de>
2021-12-07 11:50:58 +0100

    clean up and comment latency tester

--- latency_tester/main.cpp
+++ latency_tester/main.cpp
@@ -11,36 +11,47 @@
 #include <X11/extensions/XShm.h>
 #include <linux/input.h>
 
+using namespace std;
+
+// position and dimension (should only one pixel!) of the region observed by XShm
 #define WIDTH 1
 #define HEIGHT 1
 #define X 200
 #define Y 200
+
+// colors used by the test program
 #define COLOR_WHITE 0xFFFFFFFF
 #define COLOR_BLACK 0xFF000000
+
+// input event
 #define MOUSE_BUTTON_LEFT 272
 #define CLICKED 1
-#define EVENT_TYPE_CLICK_EVDEV 0
-#define EVENT_TYPE_XSHM 1
 
-using namespace std;
-
-const char* EVENT_NAMES[2] = {"click_evdev", "xshm"};
-
+// variables for xshm
 Display *display;
 Window rootWindow;
 XShmSegmentInfo shminfo;
 XImage *image;
 
-char* program_under_test;
+char* testProgramName;
+
+// as printing to stdout is slow, we store all events occuring in a list
+// and print its content in the end
+#define EVENT_TYPE_CLICK_EVDEV 0
+#define EVENT_TYPE_XSHM 1
 
 struct Event {
-    uint64_t time;
-    int iteration;
-    int event_type;
+    uint64_t time;  // event timestamp in microseconds
+    int iteration;  // iteration number to identify events belonging together
+    int event_type; // used to identify events; replaced with EVENT_NAMES when printing the log
 };
 
-vector<Event> events;
+vector<Event> events;   // event list
 
+// used to make events human readable for easier evaluation
+const char* EVENT_NAMES[2] = {"click_evdev", "xshm"};
+
+// appends event to the events list
 void logEvent(uint64_t time, int event, int iteration)
 {
     Event tempEvent = Event();
@@ -50,21 +61,29 @@ void logEvent(uint64_t time, int event, int iteration)
     events.push_back(tempEvent);
 }
 
+// called when program is finished
+// iterates the events list and prints it to stdout in CSV format
 void printLog()
 {
     cout << "time,iteration,event,program" << endl;
-    for(auto const& ev : events) {
-        cout << dec << ev.time << "," << ev.iteration << "," << EVENT_NAMES[ev.event_type] << "," << program_under_test << endl;
+    for(auto const& ev : events)
+    {
+        cout << dec <<
+            ev.time << "," <<
+            ev.iteration << "," <<
+            EVENT_NAMES[ev.event_type] << "," <<
+            testProgramName << endl;
     }
 }
 
+// get current microseconds
 uint64_t micros()
 {
     using namespace chrono;
-    uint64_t us = duration_cast<microseconds>(high_resolution_clock::now().time_since_epoch()).count();
-    return us; 
+    return duration_cast<microseconds>(high_resolution_clock::now().time_since_epoch()).count();
 }
 
+// initialize the XShm extension to be able to read one pixel from the screen
 void initXShm()
 {
     XWindowAttributes windowAttributes;
@@ -75,6 +94,7 @@ void initXShm()
     XGetWindowAttributes(display, rootWindow, &windowAttributes);
     screen = windowAttributes.screen;
 
+    // create an image object that will store the pixel data
     image = XShmCreateImage(display, DefaultVisualOfScreen(screen), DefaultDepthOfScreen(screen), ZPixmap, NULL, &shminfo, WIDTH, HEIGHT);
 
     shminfo.shmid = shmget(IPC_PRIVATE, image->bytes_per_line * image->height, IPC_CREAT|0777);
@@ -83,18 +103,22 @@ void initXShm()
     XShmAttach(display, &shminfo);
 }
 
+// detach XShm and clean up memory
+// this has to be called when the program terminates
 void closeXShm()
 {
     XShmDetach(display, &shminfo);
     XDestroyImage(image);
 }
 
+// get pixel at specified position with XShm
 unsigned int getPixelColor()
 {
     XShmGetImage(display, rootWindow, image, X, Y, 0x00ffffff);
-    return image->data[2]; // red
+    return image->data[2]; // red channel is enough for us
 }
 
+// wait until our pixel has a specified color
 void wait_for_color(unsigned int color)
 {
     while(getPixelColor() != color)
@@ -104,11 +128,13 @@ void wait_for_color(unsigned int color)
     return;
 }
 
+// do all the cleanup before terminating
 void cleanup()
 {
     closeXShm();
 }
 
+// make sure we clean up and print current logs when the program is interrupted
 void signalHandler(int sig)
 {
     printLog();
@@ -120,11 +146,13 @@ int main(int argc, char** argv)
 {
     signal(SIGINT, signalHandler);
 
+    // variables used by evdev to get input events
     int input_fd = -1;
     struct input_event inputEvent;
     int err = -1;
-    char* event_handle;
+    char* event_handle; // path to /dev/input/eventXY for the used input device
 
+    // check command line parameters
     if(argc < 3)
     {
         cerr << "Too few arguments!" << endl
@@ -136,9 +164,10 @@ int main(int argc, char** argv)
     else
     {
         event_handle = argv[1];
-        program_under_test = argv[2];
+        testProgramName = argv[2];
     }
 
+    // open input device
     input_fd = open(event_handle, O_RDONLY | O_NONBLOCK);
 
     if(input_fd == -1)
@@ -153,16 +182,19 @@ int main(int argc, char** argv)
 
     while(true)
     {
+        // read input events from the specified device
         err = read(input_fd, &inputEvent, sizeof(struct input_event));
-        if(err > -1 && inputEvent.type == EV_KEY)
+
+        // detected a left mouse click
+        if( err > -1 &&
+            inputEvent.type == EV_KEY &&
+            inputEvent.code == MOUSE_BUTTON_LEFT &&
+            inputEvent.value == CLICKED)
         {
-            if(inputEvent.code == MOUSE_BUTTON_LEFT && inputEvent.value == CLICKED)
-            {
-                logEvent(micros(), EVENT_TYPE_CLICK_EVDEV, iteration);
-                wait_for_color(COLOR_WHITE);
-                logEvent(micros(), EVENT_TYPE_XSHM, iteration);
-                iteration++;
-            }
+            logEvent(micros(), EVENT_TYPE_CLICK_EVDEV, iteration); // log input event timestamp
+            wait_for_color(COLOR_WHITE); // wait for test program to react
+            logEvent(micros(), EVENT_TYPE_XSHM, iteration); // log color change timestamp
+            iteration++;
         }
     }
 
