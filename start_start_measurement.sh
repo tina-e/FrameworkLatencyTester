0b59ad8cc2e357c2e1a102060b6c18a99b30a387
Andreas Schmid <andreas.schmid@ur.de>
2022-11-17 12:40:22 +0100

    use global variables in start script

--- start_start_measurement.sh
+++ start_start_measurement.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+DATA_DIR="2022-11-17_laptop-picom"
+ITERATIONS=100
+
 sleep 5s
-./start_measurement.sh pyqt5 default 100 test_compositor
-./start_measurement.sh pygame default 100 test_compositor
-#./start_measurement.sh tkinter default 100 test_compositor
-./start_measurement.sh OPENGL_GLEW default 100 test_compositor
-#./start_measurement.sh OPENGL_GLUT default 100 test_compositor
-#./start_measurement.sh SDL2 opengl 100 test_compositor
-#./start_measurement.sh SDL2 opengles2 100 test_compositor
-#./start_measurement.sh SDL2 software 100 test_compositor
+./start_measurement.sh pyqt5 default $ITERATIONS $DATA_DIR
+./start_measurement.sh pygame default $ITERATIONS $DATA_DIR
+./start_measurement.sh tkinter default $ITERATIONS $DATA_DIR
+./start_measurement.sh OPENGL_GLEW default $ITERATIONS $DATA_DIR
+./start_measurement.sh OPENGL_GLUT default $ITERATIONS $DATA_DIR
+./start_measurement.sh SDL2 opengl $ITERATIONS $DATA_DIR
+./start_measurement.sh SDL2 opengles2 $ITERATIONS $DATA_DIR
+./start_measurement.sh SDL2 software $ITERATIONS $DATA_DIR
