4a648cc5d06603e8f490edefb8809491757e9e39
Andreas Schmid <andreas.schmid@ur.de>
2023-01-10 15:43:29 +0100

    add primitive control script for yalmd

---
+++ arduino_clicker/control_yalmd.py
@@ -0,0 +1,40 @@
+import serial
+import io
+import time
+import sys
+import signal
+import random
+
+ser = serial.Serial('/dev/ttyACM0')  # open serial port
+ser.flushInput()
+
+def signal_handler(sig, frame):
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+time.sleep(1)
+
+ser.write('c'.encode())
+
+ser_bytes = ser.readline()
+decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
+print(decoded_bytes)
+ser_bytes = ser.readline()
+decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
+print(decoded_bytes)
+ser_bytes = ser.readline()
+decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
+print(decoded_bytes)
+
+
+while True:
+    ser.write('m'.encode())
+    try:
+        ser_bytes = ser.readline()
+        decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
+        print(decoded_bytes)
+    except:
+        print('error')
+        break
+    #time.sleep(float(random.randint(200, 2000)) / 1000.0)
