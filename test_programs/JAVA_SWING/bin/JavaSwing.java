97bca7ba4b1f0da93dc59c31c4abcd779ded1976
Vitus <vitus.maierhoefer@gmail.com>
2023-01-12 16:03:23 +0100

    Add sample JavaSwing Version

---
+++ test_programs/JAVA_SWING/bin/JavaSwing.java
@@ -0,0 +1,49 @@
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.*;
+
+public class JavaSwing implements MouseListener {
+
+    JFrame frame;
+
+    public JavaSwing() {
+        frame = new JFrame();
+        frame.setUndecorated(true);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(Color.BLACK);
+        frame.addMouseListener(this);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new JavaSwing();
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        frame.getContentPane().setBackground(Color.WHITE);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        frame.getContentPane().setBackground(Color.BLACK);
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
