9c035423137bf654e73233521f6ef58148f0d33c
Andreas Schmid <andreas.schmid@ur.de>
2023-01-12 19:58:55 +0100

    add new frameworks to start measurement

--- start_start_measurement_new.sh
+++ start_start_measurement_new.sh
@@ -2,7 +2,7 @@
 
 ITERATIONS=500
 
-DATA_DIR="2023-01-12_pc-nocomp"
+DATA_DIR="2023-01-12_pc-nocomp-2"
 #killall -9 picom
 
 #xfconf-query --channel=xfwm4 --property=/general/use_compositing --type=bool --set false
@@ -10,30 +10,42 @@ DATA_DIR="2023-01-12_pc-nocomp"
 qdbus org.kde.KWin /Compositor suspend
 
 sleep 5s
-./start_measurement_new.sh pyqt5 default $ITERATIONS $DATA_DIR
-./start_measurement_new.sh pygame default $ITERATIONS $DATA_DIR
-./start_measurement_new.sh tkinter default $ITERATIONS $DATA_DIR
-./start_measurement_new.sh OPENGL_GLEW default $ITERATIONS $DATA_DIR
-./start_measurement_new.sh OPENGL_GLUT default $ITERATIONS $DATA_DIR
-./start_measurement_new.sh SDL2 opengl $ITERATIONS $DATA_DIR
-./start_measurement_new.sh SDL2 opengles2 $ITERATIONS $DATA_DIR
-./start_measurement_new.sh SDL2 software $ITERATIONS $DATA_DIR
+./start_measurement_new.sh gtk default $ITERATIONS $DATA_DIR
+./start_measurement_new.sh JavaAWT default $ITERATIONS $DATA_DIR
+./start_measurement_new.sh JavaSwing default $ITERATIONS $DATA_DIR
+./start_measurement_new.sh pyqt6 default $ITERATIONS $DATA_DIR
+./start_measurement_new.sh wxpython default $ITERATIONS $DATA_DIR
 
-DATA_DIR="2023-01-12_pc-comp"
+#./start_measurement_new.sh pyqt5 default $ITERATIONS $DATA_DIR
+#./start_measurement_new.sh pygame default $ITERATIONS $DATA_DIR
+#./start_measurement_new.sh tkinter default $ITERATIONS $DATA_DIR
+#./start_measurement_new.sh OPENGL_GLEW default $ITERATIONS $DATA_DIR
+#./start_measurement_new.sh OPENGL_GLUT default $ITERATIONS $DATA_DIR
+#./start_measurement_new.sh SDL2 opengl $ITERATIONS $DATA_DIR
+#./start_measurement_new.sh SDL2 opengles2 $ITERATIONS $DATA_DIR
+#./start_measurement_new.sh SDL2 software $ITERATIONS $DATA_DIR
+
+DATA_DIR="2023-01-12_pc-comp-2"
 
 #xfconf-query --channel=xfwm4 --property=/general/use_compositing --type=bool --set true
 
 qdbus org.kde.KWin /Compositor resume
 
 sleep 5s
-./start_measurement_new.sh pyqt5 default $ITERATIONS $DATA_DIR
-./start_measurement_new.sh pygame default $ITERATIONS $DATA_DIR
-./start_measurement_new.sh tkinter default $ITERATIONS $DATA_DIR
-./start_measurement_new.sh OPENGL_GLEW default $ITERATIONS $DATA_DIR
-./start_measurement_new.sh OPENGL_GLUT default $ITERATIONS $DATA_DIR
-./start_measurement_new.sh SDL2 opengl $ITERATIONS $DATA_DIR
-./start_measurement_new.sh SDL2 opengles2 $ITERATIONS $DATA_DIR
-./start_measurement_new.sh SDL2 software $ITERATIONS $DATA_DIR
+./start_measurement_new.sh gtk default $ITERATIONS $DATA_DIR
+./start_measurement_new.sh JavaAWT default $ITERATIONS $DATA_DIR
+./start_measurement_new.sh JavaSwing default $ITERATIONS $DATA_DIR
+./start_measurement_new.sh pyqt6 default $ITERATIONS $DATA_DIR
+./start_measurement_new.sh wxpython default $ITERATIONS $DATA_DIR
+
+#./start_measurement_new.sh pyqt5 default $ITERATIONS $DATA_DIR
+#./start_measurement_new.sh pygame default $ITERATIONS $DATA_DIR
+#./start_measurement_new.sh tkinter default $ITERATIONS $DATA_DIR
+#./start_measurement_new.sh OPENGL_GLEW default $ITERATIONS $DATA_DIR
+#./start_measurement_new.sh OPENGL_GLUT default $ITERATIONS $DATA_DIR
+#./start_measurement_new.sh SDL2 opengl $ITERATIONS $DATA_DIR
+#./start_measurement_new.sh SDL2 opengles2 $ITERATIONS $DATA_DIR
+#./start_measurement_new.sh SDL2 software $ITERATIONS $DATA_DIR
 
 
 
