c06961f581b3d5d3181af5d92a6a13395417d49d
Andreas Schmid <andreas.schmid@ur.de>
2023-01-13 17:05:19 +0100

    add test program for Java2D

---
+++ test_programs/Java2D/Java2D.java
@@ -0,0 +1,47 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Java2D extends JFrame {
+    private Color color;
+
+    public Java2D() {
+        setSize(1920, 1080);
+        setTitle("Swing Test Application");
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setVisible(true);
+        color = new Color(0, 0, 0);
+    }
+
+    public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(color);
+        g2d.fillRect(0, 0, getWidth(), getHeight());
+    }
+
+    public void mousePressed(MouseEvent e) {
+        color = new Color(255, 255, 255);
+        repaint();
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        color = new Color(0, 0, 0);
+        repaint();
+    }
+
+    public static void main(String[] args) {
+        Java2D window = new Java2D();
+        window.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                window.mousePressed(e);
+            }
+
+            public void mouseReleased(MouseEvent e) {
+                window.mouseReleased(e);
+            }
+        });
+        GraphicsEnvironment graphics = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice device = graphics.getDefaultScreenDevice();
+        device.setFullScreenWindow(window);
+    }
+}
