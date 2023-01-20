a289a23d9c714167a69d499a8ae410c20da5f710
A-Schmid <andreas.schmid@ur.de>
2023-01-20 11:14:33 +0100

    commit before submission

--- start_measurement_new.sh
+++ start_measurement_new.sh
@@ -13,10 +13,6 @@ mkdir "data/$DATA_SUBDIRECTORY" 2> /dev/null
 ./$PATH_TEST_PROGRAM > /dev/null &
 PID_TEST_PROGRAM=$!
 
-sleep "1s"
-
-python3 calibrate_yalmd.py
-
 # could be used to move cursor to specified position
 #xdotool mousemove $x $y
 
@@ -25,6 +21,12 @@ sigint() {
 }
 
 trap sigint INT
+
+sleep "3s"
+
+python3 calibrate_yalmd.py
+
+sleep "3s"
 
 python3 control_yalmd.py "${TEST_PROGRAM}_${TEST_PARAMS}" $ITERATIONS 1 "${PATH_DATA}_fw.csv"
 
